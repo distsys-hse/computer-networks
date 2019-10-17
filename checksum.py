c7a9a28c4f7990a2cda23ac846a582fc55275be5
Aphanasiy <M79150306109@ya.ru>
2019-10-17 13:22:59 +0300

    checksum update

--- checksum.py
+++ checksum.py
@@ -1,24 +1,28 @@
 message = input("message: ")
 pol = input("polynom: ") #порождающий полином
-tp = int(input("type: "))
+tp = len(pol) - 1
 
 message += '0' * tp
 m = int(message, 2)
 p = int(pol, 2)
 it = 1 << (len(message))
 
-print(bin(m), bin(p))
-print(bin(it))
+i = 1
 
 lp = len(pol)
 
 while (it > (1 << (lp - 1))):
     while (it > m):
         it >>= 1
-    dp = p * (it >> (lp - 1))
-    print(bin(m))
-    print(bin(it))
-    print(bin(dp))
-    m ^= dp
 
-print(bin(m)[2:].lstrip('0'))
+    dp = p * (it >> (lp - 1))
+    if (dp == 0):
+        break
+    print(f"--- Round {i} ---")
+    print("M(x):", bin(m)[2:])
+    print("G(x):", bin(p)[2:])
+    print("X(x):", bin(dp)[2:])
+    m ^= dp
+    i += 1
+
+print("checksum:", bin(m)[2:].lstrip('0'))
